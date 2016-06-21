23fa2565ad2726287d1bdef07c1557d1d7902e89
Lucas GEHIN <lucas@medialta.com>
2016-06-21 16:11:58 +0200

    added french translation

---
+++ contao/languages/fr/tl_module.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_module']['opengraph_enable']    = array('Balises meta OpenGraph', 'Ajouter des balises meta OpenGraph.');
+
---
+++ contao/languages/fr/tl_page.php
@@ -0,0 +1,9 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_page']['opengraph_legend']           = 'OpenGraph';
+$GLOBALS['TL_LANG']['tl_page']['opengraph_enable']           = array('Balises meta OpenGraph', 'Ajouter des balises meta OpenGraph.');
+$GLOBALS['TL_LANG']['tl_page']['opengraph_twitter_site']     = array('Site Twitter', '@utilisateur du site, requis pour la bonne intégration de la Twitter Card');
+$GLOBALS['TL_LANG']['tl_page']['opengraph_twitter_card']     = array('Twitter Card', 'Le type de Twitter Card <a href="https://dev.twitter.com/cards/types">, par défaut summary_large_image');
+$GLOBALS['TL_LANG']['tl_page']['opengraph_pageimage']        = array('Utilise les images définies par l\'extension pageimage', 'Utilise les images définies par l\'extension pageimage');
+$GLOBALS['TL_LANG']['tl_page']['opengraph_img_recursive']    = array('Intégrer les images récursivement', 'Intégre les images depuis la page parent si aucune image n\'est renseignée.');
+$GLOBALS['TL_LANG']['tl_page']['opengraph_image']            = array('Balise meta OpenGraph image', 'Ajoute cette image comme balise meta OpenGraph image.');
